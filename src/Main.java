70256721bae1e4a3ab43839e887154fc963743ab
vihdobalacobaco <vitoriajemima2007@gmail.com>
2023-06-15 12:26:44 -0300

    Natividade

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,18 @@ public class Main {
     public static void main(String[] args){
 
     //Criar objetos de passseio
-    Pessoa p1 Pessoa();
-    Pessoa p2;
-    p2 = new Pessoa(); //Construcao
-    //,,,
+    Pessoa p1 new Pessoa();
+    Pessoa p2 = new Pessoa(); //Construcao
+        //,,,
     p1.none = "Eliot Page";
+    p2.none = "Vi";
+
+    p1.rua = "rua Platina";
+    p1.cidade = "Indaiatuba";
+    p1.estado = "Sao Paulo";
+
+    p2.rua = "rua Platina";
+    p2.cidade = "Indaiatuba";
+    p2.estado = "Sao Paulo";
+
 }
--- src/Pessoa.java
+++ src/Pessoa.java
@@ -1,8 +1,18 @@
 public class Pessoa {
+    public String none;
     // Atributos
     String nome;
-
-    //Métodos
+    int telefone;
+    int RG;
+    int CPF;
+    int telecontato;
+    
+    String rua;
+    
+    String estado;
+    
+    String cidade;
+    
     public void pesquisarEmpresa(){
 
     }
