8d34250f313bc7735a179ef9bbb8767d9a2686b4
vihdobalacobaco <vitoriajemima2007@gmail.com>
2023-06-15 11:55:44 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+
+    //Criar objetos de passseio
+    Pessoa p1 Pessoa();
+    Pessoa p2;
+    p2 = new Pessoa(); //Construcao
+    //,,,
+    p1.none = "Eliot Page";
+}
